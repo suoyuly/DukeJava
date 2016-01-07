a6ab4eec9de1ebdce38eb12583dd895062fd1c01
Yao-Luo <ly1987510@gmail.com>
2016-01-07 16:27:28 -0500

    2nd test

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is a test for git hub");
+		System.out.println("This is a second test for git hub");
 
 	}
 
